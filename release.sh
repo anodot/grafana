43ffeeafb267108fd5ad85ae14a9ad158ada87d6
Andrii Ulianenko <andrii.ulianenko@anodot.com>
2024-07-08 23:59:51 +0100

    revert change

--- release.sh
+++ release.sh
@@ -1,13 +1,10 @@
 #! /bin/bash
 mv anodot-datasource dist
-yarn build
-# export GRAFANA_API_KEY=<Your Grafana Api key>
-npx @grafana/toolkit plugin:sign
+# mage -v
+npm run build
+# export GRAFANA_ACCESS_POLICY_TOKEN=<Your token>
+npx @grafana/sign-plugin@latest
 mv dist/ anodot-datasource
 zip anodot-datasource-1.0.3.zip anodot-datasource -r
 md5 anodot-datasource-1.0.3.zip> md5Checksum.txt
 mv anodot-datasource dist
-
-
-
-
