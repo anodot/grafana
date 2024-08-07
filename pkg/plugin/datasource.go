abf19c49dcc490a92557eeb60ee3dc548514c1de
Andrii Ulianenko <andrii.ulianenko@anodot.com>
2024-08-08 00:50:54 +0100

    Refactor go for review

--- pkg/plugin/datasource.go
+++ pkg/plugin/datasource.go
@@ -13,45 +13,51 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
-	"github.com/anodot/grafana-datasource/pkg/models"
 )
 
 var tokenStorage string = ""
 
 // Make sure Datasource implements required interfaces. This is important to do
 // since otherwise we will only get a not implemented error response from plugin in
-// runtime. In this example datasource instance implements backend.QueryDataHandler,
-// backend.CheckHealthHandler interfaces. Plugin should not implement all these
+// runtime. Plugin should not implement all these
 // interfaces - only those which are required for a particular task.
 var (
-	_ backend.QueryDataHandler      = (*Datasource)(nil)
-	_ backend.CheckHealthHandler    = (*Datasource)(nil)
 	_ backend.CallResourceHandler    = (*Datasource)(nil)
 	_ instancemgmt.InstanceDisposer = (*Datasource)(nil)
 )
-
-// NewDatasource creates a new datasource instance.
-func NewDatasource(_ context.Context, _ backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
-	return &Datasource{}, nil
-}
-
 // Datasource is an example datasource which can respond to data queries, reports
 // its health and has streaming skills.
-type Datasource struct{}
+type Datasource struct {
+    url         string
+    apiPostfix  string
+    refreshToken string
+}
+
+// NewDatasource creates a new datasource instance.
+func NewDatasource(_ context.Context, settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+	var jsonData struct {
+            URL     string `json:"url"`
+            POSTFIX string `json:"apiPostfix"`
+        }
+
+        if err := json.Unmarshal(settings.JSONData, &jsonData); err != nil {
+            return nil, err
+        }
+
+        refreshToken := settings.DecryptedSecureJSONData["token"]
+
+        return &Datasource{
+            url:         jsonData.URL,
+            apiPostfix:  jsonData.POSTFIX,
+            refreshToken: refreshToken,
+        }, nil
+}
 
 // Dispose here tells plugin SDK that plugin wants to clean up resources when a new instance
 // created. As soon as datasource settings change detected by SDK old datasource instance will
 // be disposed and a new one will be created using NewSampleDatasource factory function.
 func (d *Datasource) Dispose() {
 	// Clean up datasource instance resources.
-}
-
-// QueryData handles multiple queries and returns multiple responses.
-// req contains the queries []DataQuery (where each query contains RefID as a unique identifier).
-// The QueryDataResponse contains a map of RefID to the response for each query, and each response
-// contains Frames ([]*Frame).
-func (d *Datasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	return backend.NewQueryDataResponse(), nil
 }
 
 type queryModel struct{}
@@ -84,52 +90,14 @@ func (d *Datasource) query(_ context.Context, pCtx backend.PluginContext, query 
 	return response
 }
 
-// CheckHealth handles health checks sent from Grafana to the plugin.
-// The main use case for these health checks is the test button on the
-// datasource configuration page which allows users to verify that
-// a datasource is working as expected.
-func (d *Datasource) CheckHealth(_ context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-	res := &backend.CheckHealthResult{}
-	config, err := models.LoadPluginSettings(*req.PluginContext.DataSourceInstanceSettings)
-
-	if err != nil {
-		res.Status = backend.HealthStatusError
-		res.Message = "Unable to load settings"
-		return res, nil
-	}
-
-	if config.Secrets.ApiKey == "" {
-		res.Status = backend.HealthStatusError
-		res.Message = "API key is missing"
-		return res, nil
-	}
-
-	return &backend.CheckHealthResult{
-		Status:  backend.HealthStatusOk,
-		Message: "Data source is working",
-	}, nil
-}
-
 func (d *Datasource) CallResource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
-	var jsonData struct {
-		URL     string `json:"url"`
-		POSTFIX string `json:"apiPostfix"`
-	}
-
-	if err := json.Unmarshal(req.PluginContext.DataSourceInstanceSettings.JSONData, &jsonData); err != nil {
-		return err
-	}
-
 	if req.Path == "access-token" || tokenStorage == "" {
-		// Get the API token with refresh token from Datasource config
-		refreshToken := req.PluginContext.DataSourceInstanceSettings.DecryptedSecureJSONData["token"]
-
-		payload, err := json.Marshal(map[string]string{"refreshToken": refreshToken})
+		payload, err := json.Marshal(map[string]string{"refreshToken": d.refreshToken})
 		if err != nil {
 			return err
 		}
 
-		authReq, err := http.NewRequest("POST", jsonData.URL+"/api/v2/access-token", bytes.NewReader(payload))
+		authReq, err := http.NewRequest("POST", d.url+"/api/v2/access-token", bytes.NewReader(payload))
 		if err != nil {
 			return err
 		}
@@ -165,7 +133,7 @@ func (d *Datasource) CallResource(ctx context.Context, req *backend.CallResource
 		payload = []byte("")
 	}
 
-	authReq, err := http.NewRequest(req.Method, jsonData.URL+jsonData.POSTFIX+req.URL, bytes.NewReader(payload))
+	authReq, err := http.NewRequest(req.Method, d.url+d.apiPostfix+req.URL, bytes.NewReader(payload))
 	if err != nil {
 		return err
 	}
